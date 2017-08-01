f1720e5f8583d098aee3788d6ec1f777f15f5a2c
Maria Franceschi <mia.franceschi@gmail.com>
2017-08-01 13:21:53 -0400

    added rover position

--- mars/rover.rb
+++ mars/rover.rb
@@ -1,3 +1,5 @@
 class Rover
 
+  attr_accessor :position
+
 end
--- mars/rover_test.rb
+++ mars/rover_test.rb
@@ -4,7 +4,11 @@ require_relative 'rover'
 
 class MyTest < Test::Unit::TestCase
 
-  def test_initalize_rover
+  def test_position
     rover = Rover.new
+    position = {x: 1, y: 2}
+    rover.position = position
+
+    assert_equal position, rover.position
   end
 end
