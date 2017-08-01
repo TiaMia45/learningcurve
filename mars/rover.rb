101a76ba617e3a3667c0fce8e60fba03ea13d449
Maria Franceschi <mia.franceschi@gmail.com>
2017-08-01 14:34:04 -0400

    cleaned up rotation code

--- mars/rover.rb
+++ mars/rover.rb
@@ -4,16 +4,21 @@ class Rover
 
   DIRECTIONS = [:n, :e, :s, :w]
 
-
   def rotate_right
-      current_direction_index = DIRECTIONS.index(@orientation)
-      @orientation = DIRECTIONS[(current_direction_index + 1) % DIRECTIONS.count]
+      @orientation = increment_direction(1)
   end
 
   def rotate_left
-      current_direction_index = DIRECTIONS.index(@orientation)
-      @orientation = DIRECTIONS[(current_direction_index - 1) % DIRECTIONS.count]
+    @orientation = increment_direction(-1)
   end
 
+  def increment_direction(val)
+    new_index = (current_direction_index + val) % DIRECTIONS.count
+    DIRECTIONS[new_index]
+  end
+
+  def current_direction_index
+    DIRECTIONS.index(@orientation)
+  end
 
 end
